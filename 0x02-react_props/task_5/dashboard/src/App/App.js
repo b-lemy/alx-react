ee4d10ad9d24b38a2366a2ed87cbecdc1edf8b63
b-lemy <lemabrian1234@gmail.com>
2023-07-31 23:07:50 +0300

    react props final

--- 0x02-react_props/task_5/dashboard/src/App/App.js
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -7,28 +7,13 @@ import "./App.css";
 import CourseList from "../CourseList/CourseList";
 import PropTypes from "prop-types";
 
-const listCourses = [
-    { id: 1, name: "ES6", credit: 60 },
-    { id: 2, name: "Webpack", credit: 20 },
-    { id: 3, name: "React", credit: 40 },
-];
-
-const listNotifications = [
-    { id: 1, type: "default", value: "New course available" },
-    { id: 2, type: "urgent", value: "New resume available" },
-    { id: 3, type: "urgent", html: getLatestNotification() },
-];
-
 function App(isLoggedIn) {
     return (
         <React.Fragment>
             <Notifications />
             <div className="App">
-                <div className="heading-section">
-                    <Notifications listNotifications={listNotifications} />
-                    <Header />
-                </div>
-                {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+                <Header />
+                {isLoggedIn ? <CourseList/> : <Login/>}
                 <Footer />
             </div>
         </React.Fragment>
