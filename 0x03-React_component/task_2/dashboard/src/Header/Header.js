2e9b2ca93f44e528f3caf81aa1e29286d76b4141
b-lemy <lemabrian1234@gmail.com>
2023-08-03 19:09:31 +0300

    intial class react components

---
+++ 0x03-React_component/task_2/dashboard/src/Header/Header.js
@@ -0,0 +1,15 @@
+import logo from "../assets/holberton-logo.jpg";
+import "./Header.css";
+import React from "react";
+const Header = () => {
+    return (
+        <>
+            <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>School dashboard</h1>
+            </div>
+        </>
+    )
+}
+
+export default Header
